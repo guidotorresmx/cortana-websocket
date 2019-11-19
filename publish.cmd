e77136b5f229b7190c90dfbfb85c13b4d38e0a2d
Marco Adrián Guido <mguido@ndscognitivelabs.com>
2019-11-19 10:06:09 -0600

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=bottestprime20-Web-Deploy.pubxml -p:Password=qHg4rR4pjPLJeiBC1XDzDuJp4FLFNSLmdeZxTSBNFH0ldX0mnrht6uyGz5kL
+
